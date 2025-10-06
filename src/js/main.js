7daa6d16cc66de63b8eba0bada04b5f54060fa64
Henrique Granatto <henrique.ramires.granatto@gmail.com>
2025-10-06 15:02:45 -0300

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
-// Import our custom CSS
 import '../scss/styles.scss'
-
-// Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
+import { JSONFilePreset } from 'lowdb/node'
+
+const defaultData = { posts: [] }
+const db = await JSONFilePreset('db.json', defaultData)
+
+await db.update(({ posts }) => posts.push('hello world'))
+
+db.data.posts.push('hello world')
+await db.write()
