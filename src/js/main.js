97c57611ae50ce6e205c899bbce22ba76da01b59
Henrique Granatto <henrique.ramires.granatto@gmail.com>
2025-10-08 16:11:46 -0300

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,8 @@ function saveBudget() {
     localStorage.setItem('budget', budgetValue)
 }
 
-function savePlataform() {
+function savePlataform(plataformName) {
     const id = JSON.parse(localStorage.getItem('plataforms')).length
-    const plataformName = document.querySelector('#savePlataformName').value
 
     let plataforms = JSON.parse(localStorage.getItem('plataforms'));
 
@@ -21,10 +20,12 @@ function savePlataform() {
     localStorage.setItem('plataforms', JSON.stringify(plataforms));
 
     showPlataformsTable()
+
+    return id
 }
 
 function saveTeam() {
-    const teamPlataform = document.querySelector('#saveTeamPlataform').value
+    const teamPlataformName = document.querySelector('#saveTeamPlataform').value
     const teamNameA = document.querySelector('#saveTeamNameA').value
     const oddA = document.querySelector('#saveOddA').value
     const teamNameB = document.querySelector('#saveTeamNameB').value
@@ -33,25 +34,132 @@ function saveTeam() {
     let teams = JSON.parse(localStorage.getItem('teams')) || [];
     let id = teams.length  > 0 ? teams.length : 1;
 
+    const plataform = savePlataform(teamPlataformName)
+
     teams.push({
         id: id,
-        plataform: teamPlataform,
+        type: 1,
+        odd: oddA,
         name: teamNameA,
-        odd: oddA
+        plataform: plataform,
     });
 
     id++;
 
     teams.push({
         id: id,
-        plataform: teamPlataform,
+        type: 2,
+        odd: oddB,
         name: teamNameB,
-        odd: oddB
+        plataform: plataform,
     });
 
     localStorage.setItem('teams', JSON.stringify(teams));
 
+    generateCombinations()
+
     showTeamsTable()
+}
+
+function generateCombinations() {
+    const teams = JSON.parse(localStorage.getItem('teams')) || []
+
+    const type1 = teams.filter(t => Number(t.type) === 1)
+    const type2 = teams.filter(t => Number(t.type) === 2)
+
+    const combinations = []
+
+    type1.forEach(t1 => {
+        type2.forEach(t2 => {
+            combinations.push({
+                teamA: t1.id,
+                teamB: t2.id,
+                oddA: t1.odd,
+                oddB: t2.odd,
+                plataformA: t1.plataform,
+                plataformB: t2.plataform
+            })
+        })
+    })
+
+    localStorage.setItem('combinations', JSON.stringify(combinations))
+    showCombinations(combinations)
+
+    return combinations
+}
+
+function showCombinations(combinations) {
+    const tbody = document.querySelector('#combinationsTable')
+    if (!tbody) return
+
+    const teams = JSON.parse(localStorage.getItem('teams')) || []
+    const plataforms = JSON.parse(localStorage.getItem('plataforms')) || []
+
+    let rows = ''
+
+    combinations.forEach((c, idx) => {
+        const teamA = teams.find(t => Number(t.id) === Number(c.teamA)) || {}
+        const teamB = teams.find(t => Number(t.id) === Number(c.teamB)) || {}
+
+        const plataformA = (plataforms.find(p => Number(p.id) === Number(c.plataformA)) || {}).name || ''
+        const plataformB = (plataforms.find(p => Number(p.id) === Number(c.plataformB)) || {}).name || ''
+
+        const oddA = Number(c.oddA) || 0
+        const oddB = Number(c.oddB) || 0
+
+        const surebetValue = calcSurebetValue(oddA, oddB)
+        const isArbitrage = isSurebet(surebetValue)
+        const margin = calcMarginPercent(surebetValue)
+        const stakes = calcStakes(oddA, oddB)
+
+        rows += `
+            <tr>
+                <td>${idx + 1}</td>
+                <td>A: ${plataformA} / B: ${plataformB}</td>
+                <td>${teamA.name || ''}</td>
+                <td>${oddA}</td>
+                <td>${teamB.name || ''}</td>
+                <td>${oddB}</td>
+                <td>${isArbitrage ? `Yes (${margin}%)` : 'No'}</td>
+            </tr>
+        `
+    })
+
+    tbody.innerHTML = rows
+}
+
+function calcSurebetValue(oddA, oddB) {
+    if (!(oddA > 0) || !(oddB > 0)) return Infinity
+    return (1 / oddA) + (1 / oddB)
+}
+
+function isSurebet(surebetValue) {
+    return surebetValue < 1
+}
+
+function calcMarginPercent(surebetValue) {
+    if (!isSurebet(surebetValue)) return '0.00'
+    return ((1 - surebetValue) * 100).toFixed(2)
+}
+
+function calcStakes(oddA, oddB, totalStake = 100) {
+    const invA = oddA > 0 ? 1 / oddA : 0
+    const invB = oddB > 0 ? 1 / oddB : 0
+    const sumInv = invA + invB
+    if (sumInv === 0) return { stakeA: 0, stakeB: 0 }
+
+    const stakeA = (totalStake * invA) / sumInv
+    const stakeB = (totalStake * invB) / sumInv
+
+    const retA = stakeA * oddA
+    const retB = stakeB * oddB
+
+    return {
+        stakeA: Number(stakeA.toFixed(2)),
+        stakeB: Number(stakeB.toFixed(2)),
+        retA: Number(retA.toFixed(2)),
+        retB: Number(retB.toFixed(2))
+    }
 }
 
 function showPlataformsTable() {
@@ -59,22 +167,19 @@ function showPlataformsTable() {
 
     document.querySelector('#plataformsTable').innerHTML = ''
 
-    localStorage.setItem('teams', JSON.stringify(
-        JSON.parse(
-            localStorage.getItem('plataforms')).forEach(plataform => {
-                tableRows += `
-                    <tr>
-                        <td>${plataform.id}</td>
-                        <td>${plataform.name}</td>
-                        <td>
-                            <button type="button" class="btn btn-primary" onclick="editPlataform(${plataform.id})">Editar</button>
-                            <button type="button" class="btn btn-primary" onclick="deletePlataform(${plataform.id})">Deletar</button>
-                        </td>
-                    </tr>
-                `    
-            }
-        )
-    ))
+    const plataforms = JSON.parse(localStorage.getItem('plataforms')) || []
+    plataforms.forEach(plataform => {
+        tableRows += `
+            <tr>
+                <td>${plataform.id}</td>
+                <td>${plataform.name}</td>
+                <td>
+                    <button type="button" class="btn btn-primary" onclick="editPlataform(${plataform.id})">Editar</button>
+                    <button type="button" class="btn btn-primary" onclick="deletePlataform(${plataform.id})">Deletar</button>
+                </td>
+            </tr>
+        `
+    })
 
     document.querySelector('#plataformsTable').innerHTML = tableRows
 }
@@ -84,22 +189,26 @@ function showTeamsTable() {
 
     document.querySelector('#teamsTable').innerHTML = ''
 
-    JSON.parse(localStorage.getItem('teams')).forEach(team => {
+    const teams = JSON.parse(localStorage.getItem('teams')) || []
+    const plataforms = JSON.parse(localStorage.getItem('plataforms')) || []
+
+    teams.forEach(team => {
+        const plataformName = (plataforms.find(p => Number(p.id) === Number(team.plataform)) || {}).name || ''
         tableRows += `
             <tr>
                 <td>${team.id}</td>
-                <td>${localStorage.getItem('plataforms')[team.plataform].name}</td>
+                <td>${plataformName}</td>
                 <td>${team.name}</td>
                 <td>${team.odd}</td>
                 <td>
-                    <button type="button" class="btn btn-primary" onclick="editPlataform(${team.id})">Editar</button>
-                    <button type="button" class="btn btn-primary" onclick="deletePlataform(${team.id})">Deletar</button>
+                    <button type="button" class="btn btn-primary" onclick="editTeam(${team.id})">Editar</button>
+                    <button type="button" class="btn btn-primary" onclick="deleteTeam(${team.id})">Deletar</button>
                 </td>
             </tr>
-        `    
+        `
     });
 
-    document.querySelector('#plataformsTable').innerHTML = tableRows
+    document.querySelector('#teamsTable').innerHTML = tableRows
 }
 
 localStorage.clear()
@@ -110,4 +219,7 @@ localStorage.setItem('combinations', JSON.stringify([]))
 
 document.querySelector('#saveTeamBtn').addEventListener('click', saveTeam)
 document.querySelector('#saveBudgetBtn').addEventListener('click', saveBudget)
-document.querySelector('#savePlataformBtn').addEventListener('click', savePlataform)
+document.querySelector('#savePlataformBtn').addEventListener('click', () => {
+    const name = document.querySelector('#savePlataformName') ? document.querySelector('#savePlataformName').value : ''
+    if (name) savePlataform(name)
+})
